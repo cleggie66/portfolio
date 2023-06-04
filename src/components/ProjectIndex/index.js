f45cf461ebbf6863454a5a9a0be74775686c1187
Caleb Cleghorn <caleb@cleghorn.org>
2023-06-03 21:15:27 -0500

    added link functionality to projects

--- src/components/ProjectIndex/index.js
+++ src/components/ProjectIndex/index.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
 
 import express from "../../media/icons/express.png"
 import flask from "../../media/icons/flask.png"
@@ -11,7 +12,6 @@ import redux from "../../media/icons/redux.png"
 import sequelize from "../../media/icons/sequelize.png"
 import sqla from "../../media/icons/sqla.png"
 
-
 import "./ProjectIndex.css"
 import "./ProjectIndexIcons.css"
 
@@ -20,59 +20,73 @@ function ProjectIndex() {
     const [project1, setProject1] = useState("project-preview-active");
     const [project2, setProject2] = useState("project-preview-idle");
     const [project3, setProject3] = useState("project-preview-idle");
-
+    const [projectLink, setProjectLink] = useState("https://my-care.onrender.com");
 
     return (
         <div className="project-index">
             <div className="projects-list">
                 <h2>Projects</h2>
                 <hr />
-                <div
-                    className="project-line"
-                    onMouseEnter={() => {
-                        setProject1("project-preview-active")
-                        setProject2("project-preview-idle")
-                        setProject3("project-preview-idle")
-                    }}>
-                    <h3>MyCare</h3>
-                    <img src={python} alt="icon" className="icon1" />
-                    <img src={react} alt="icon" className="icon2" />
-                    <img src={redux} alt="icon" className="icon3" />
-                    <img src={flask} alt="icon" className="icon4" />
-                    <img src={sqla} alt="icon" className="icon5" />
-                </div>
+                <a href={projectLink} target="_blank" rel="noopener noreferrer">
+                    <div
+                        className="project-line"
+                        onMouseEnter={() => {
+                            setProject1("project-preview-active");
+                            setProject2("project-preview-idle");
+                            setProject3("project-preview-idle");
+                            setProjectLink("https://my-care.onrender.com");
+                        }}>
+                        <h3 className="project-title">MyCare</h3>
+                        <img src={python} alt="icon" className="icon1" />
+                        <img src={react} alt="icon" className="icon2" />
+                        <img src={redux} alt="icon" className="icon3" />
+                        <img src={flask} alt="icon" className="icon4" />
+                        <img src={sqla} alt="icon" className="icon5" />
+                    </div>
+                </a>
                 <hr />
-                <div
-                    className="project-line"
-                    onMouseEnter={() => {
-                        setProject1("project-preview-idle")
-                        setProject2("project-preview-active")
-                        setProject3("project-preview-idle")
-                    }}>
-                    <h3>Banter</h3>
-                    <img src={python} alt="icon" className="icon1" />
-                    <img src={react} alt="icon" className="icon2" />
-                    <img src={redux} alt="icon" className="icon3" />
-                    <img src={flask} alt="icon" className="icon4" />
-                    <img src={sqla} alt="icon" className="icon5" />
-                </div>
+                <a href={projectLink} target="_blank" rel="noopener noreferrer">
+                    <div
+                        className="project-line"
+                        onMouseEnter={() => {
+                            setProject1("project-preview-idle");
+                            setProject2("project-preview-active");
+                            setProject3("project-preview-idle");
+                            setProjectLink("https://banter-k9ts.onrender.com/");
+
+                        }}>
+                        <h3 className="project-title">Banter</h3>
+                        <img src={python} alt="icon" className="icon1" />
+                        <img src={react} alt="icon" className="icon2" />
+                        <img src={redux} alt="icon" className="icon3" />
+                        <img src={flask} alt="icon" className="icon4" />
+                        <img src={sqla} alt="icon" className="icon5" />
+                    </div>
+                </a>
                 <hr />
-                <div
-                className="project-line"
-                    onMouseEnter={() => {
-                        setProject1("project-preview-idle")
-                        setProject2("project-preview-idle")
-                        setProject3("project-preview-active")
-                    }}>
-                    <h3>AirBn2B</h3>
-                    <img src={javascript} alt="icon" className="icon1" />
-                    <img src={express} alt="icon" className="icon2" />
-                    <img src={react} alt="icon" className="icon3" />
-                    <img src={redux} alt="icon" className="icon4" />
-                    <img src={sequelize} alt="icon" className="icon5" />
-                </div>
+                <a href={projectLink} target="_blank" rel="noopener noreferrer">
+                    <div
+                        className="project-line"
+                        onMouseEnter={() => {
+                            setProject1("project-preview-idle");
+                            setProject2("project-preview-idle");
+                            setProject3("project-preview-active");
+                            setProjectLink("https://caleb-airbnb.onrender.com/");
+
+                        }}>
+                        <h3 className="project-title">AirBn2B</h3>
+                        <img src={javascript} alt="icon" className="icon1" />
+                        <img src={express} alt="icon" className="icon2" />
+                        <img src={react} alt="icon" className="icon3" />
+                        <img src={redux} alt="icon" className="icon4" />
+                        <img src={sequelize} alt="icon" className="icon5" />
+                    </div>
+                </a>
             </div>
+            <a href={projectLink} target="_blank" rel="noopener noreferrer">
             <div className="project-image-container">
+                <p className="call-to-action">VISIT WEBSITE</p>
+                <div className="white-cover" />
                 <img src={"https://blog.artsper.com/wp-content/uploads/2022/04/New-Featured-Image-1200-x-675-6.jpg"}
                     alt="project preview"
                     className={`${project1}`}
@@ -86,6 +100,7 @@ function ProjectIndex() {
                     className={`${project3}`}
                 />
             </div>
+            </a>
         </div>
     )
 }
