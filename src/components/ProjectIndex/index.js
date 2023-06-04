e5741dea089e182d25a53836064faa7badd9a898
Caleb Cleghorn <caleb@cleghorn.org>
2023-06-03 20:21:12 -0500

    all icons added

--- src/components/ProjectIndex/index.js
+++ src/components/ProjectIndex/index.js
@@ -1,5 +1,17 @@
 import { useState } from "react"
+
+import express from "../../media/icons/express.png"
+import flask from "../../media/icons/flask.png"
 import javascript from "../../media/icons/javascript.png"
+import node from "../../media/icons/node.png"
+import postgresql from "../../media/icons/postgresql.png"
+import python from "../../media/icons/python.png"
+import react from "../../media/icons/react.png"
+import redux from "../../media/icons/redux.png"
+import sequelize from "../../media/icons/sequelize.png"
+import sqla from "../../media/icons/sqla.png"
+
+
 import "./ProjectIndex.css"
 import "./ProjectIndexIcons.css"
 
@@ -15,42 +27,50 @@ function ProjectIndex() {
             <div className="projects-list">
                 <h2>Projects</h2>
                 <hr />
-                <div className="project-line">
-                    <h3
-                        onMouseEnter={() => {
-                            setProject1("project-preview-active")
-                            setProject2("project-preview-idle")
-                            setProject3("project-preview-idle")
-                        }}
-                    >
-                        MyCare
-                    </h3>
-                    <img src={javascript} alt="icon" className="icon1" />
-                    <img src={javascript} alt="icon" className="icon2" />
-                    <img src={javascript} alt="icon" className="icon3" />
-                    <img src={javascript} alt="icon" className="icon4" />
-                    <img src={javascript} alt="icon" className="icon5" />
+                <div
+                    className="project-line"
+                    onMouseEnter={() => {
+                        setProject1("project-preview-active")
+                        setProject2("project-preview-idle")
+                        setProject3("project-preview-idle")
+                    }}>
+                    <h3>MyCare</h3>
+                    <img src={python} alt="icon" className="icon1" />
+                    <img src={react} alt="icon" className="icon2" />
+                    <img src={redux} alt="icon" className="icon3" />
+                    <img src={flask} alt="icon" className="icon4" />
+                    <img src={sqla} alt="icon" className="icon5" />
                 </div>
                 <hr />
-                <h3
+                <div
+                    className="project-line"
                     onMouseEnter={() => {
                         setProject1("project-preview-idle")
                         setProject2("project-preview-active")
                         setProject3("project-preview-idle")
-                    }}
-                >
-                    Banter
-                </h3>
+                    }}>
+                    <h3>Banter</h3>
+                    <img src={python} alt="icon" className="icon1" />
+                    <img src={react} alt="icon" className="icon2" />
+                    <img src={redux} alt="icon" className="icon3" />
+                    <img src={flask} alt="icon" className="icon4" />
+                    <img src={sqla} alt="icon" className="icon5" />
+                </div>
                 <hr />
-                <h3
+                <div
+                className="project-line"
                     onMouseEnter={() => {
                         setProject1("project-preview-idle")
                         setProject2("project-preview-idle")
                         setProject3("project-preview-active")
-                    }}
-                >
-                    AirBn2B
-                </h3>
+                    }}>
+                    <h3>AirBn2B</h3>
+                    <img src={javascript} alt="icon" className="icon1" />
+                    <img src={express} alt="icon" className="icon2" />
+                    <img src={react} alt="icon" className="icon3" />
+                    <img src={redux} alt="icon" className="icon4" />
+                    <img src={sequelize} alt="icon" className="icon5" />
+                </div>
             </div>
             <div className="project-image-container">
                 <img src={"https://blog.artsper.com/wp-content/uploads/2022/04/New-Featured-Image-1200-x-675-6.jpg"}
