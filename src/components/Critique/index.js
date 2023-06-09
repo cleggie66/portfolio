dd8d416a8e88d305f37781bde9e3cc5d17f33213
Caleb Cleghorn <caleb@cleghorn.org>
2023-06-09 17:29:44 -0500

    added email functionality

--- src/components/Critique/index.js
+++ src/components/Critique/index.js
@@ -1,6 +1,49 @@
+import { useEffect, useRef, useState } from "react";
+import emailjs from '@emailjs/browser/es';
 import "./Critique.css"
 
 function Critique({ visibility }) {
+    const form = useRef();
+    const [hasSubmitted, setHasSubmitted] = useState(false);
+    const [project, setProject] = useState("");
+    const [critique, setCritique] = useState("");
+    const [buttonClass, setButtonClass] = useState("critique-button");
+    const [buttonText, setButtonText] = useState("Send It");
+    const [errors, setErrors] = useState({});
+
+    useEffect(() => {
+        const errorsObj = {}
+        if (project.length === 0) errorsObj.project = "Cannot be blank"
+        if (critique.length === 0) errorsObj.critique = "Cannot be blank"
+        setErrors(errorsObj)
+
+    }, [project, critique])
+
+    const handleButton = () => {
+        setButtonClass("critique-button-sent");
+        setButtonText("Sent!");
+
+        setTimeout(() => {
+            setButtonClass("critique-button");
+            setButtonText("Send It")
+        }, 3000);
+    }
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        setHasSubmitted(true);
+
+        if (Object.values(errors).length === 0) {
+            emailjs.sendForm('service_3gfds3h', 'template_2r63new', form.current, 'vdYMyjgLbJ0XXfGXs');
+
+            handleButton();
+            setProject("");
+            setCritique("");
+            setErrors({});
+            setHasSubmitted(false);
+        }
+    }
 
     return (
         <div className={visibility}>
@@ -8,19 +51,35 @@ function Critique({ visibility }) {
                 <h2>
                     Send me a Critique!
                 </h2>
-                <form className="critique-form">
-                    <select>
-                        <option disabled selected>Select Project</option>
+                <form className="critique-form" ref={form} onSubmit={sendEmail}>
+                    <select
+                        name="project"
+                        value={project}
+                        onChange={(e) => setProject(e.target.value)}
+                    >
+                        <option disabled selected value="">Select Project</option>
                         <option>MyCare</option>
                         <option>Banter</option>
                         <option>AirBn2B</option>
                         <option>Portfolio Site</option>
                         <option>Other</option>
                     </select>
-                    <textarea id="story" name="story"
-                        rows="5" cols="33" placeholder="Your Feedback">
+                    {hasSubmitted && (<p className="error">{errors.project}</p>)}
+                    <textarea
+                        name="critique"
+                        rows="5"
+                        cols="33"
+                        placeholder="Your Feedback"
+                        value={critique}
+                        onChange={(e) => setCritique(e.target.value)}
+                    >
                     </textarea>
-                    <button>Send it</button>
+                    {hasSubmitted && (<p className="error">{errors.critique}</p>)}
+                    <button
+                        disabled={hasSubmitted && Object.values(errors).length !== 0}
+                        className={buttonClass}>
+                        {buttonText}
+                    </button>
                 </form>
             </div>
         </div>
--- src/components/ReachOut/index.js
+++ src/components/ReachOut/index.js
@@ -1,15 +1,91 @@
+import React, { useEffect, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser/es';
 import "./ReachOut.css"
 
 function ReachOut({ visibility }) {
+    const form = useRef();
+    const [hasSubmitted, setHasSubmitted] = useState(false);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [buttonClass, setButtonClass] = useState("reach-out-button");
+    const [buttonText, setButtonText] = useState("Send It");
+    const [errors, setErrors] = useState({});
+
+    useEffect(() => {
+        const errorsObj = {}
+        if (name.length === 0) errorsObj.name = "Cannot be blank"
+        if (email.length === 0) errorsObj.email = "Cannot be blank"
+        if (message.length === 0) errorsObj.message = "Cannot be blank"
+        setErrors(errorsObj)
+
+    }, [name, email, message])
+
+    const handleButton = () => {
+        setButtonClass("reach-out-button-sent");
+        setButtonText("Sent!");
+
+        setTimeout(() => {
+            setButtonClass("reach-out-button");
+            setButtonText("Send It")
+        }, 3000);
+    }
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        setHasSubmitted(true);
+
+        if (Object.values(errors).length === 0) {
+            emailjs.sendForm('service_3gfds3h', 'template_qrgr3te', form.current, 'vdYMyjgLbJ0XXfGXs');
+
+            handleButton();
+            setName("");
+            setEmail("");
+            setMessage("");
+            setErrors({});
+            setHasSubmitted(false);
+        }
+    }
+
     return (
         <div className={visibility}>
             <div className="reach-out-page">
                 <h2>Get In Touch</h2>
-                <form className="reach-out-form">
-                    <input type="text" name="name" id="name" placeholder="Name" />
-                    <input type="email" name="email" id="email" placeholder="Email" />
-                    <textarea name="message" id="message" placeholder="Message" rows="4"></textarea>
-                    <button>Send it</button>
+                <form className="reach-out-form" ref={form} onSubmit={sendEmail}>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    {hasSubmitted && (<p className="error">{errors.name}</p>)}
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    {hasSubmitted && (<p className="error">{errors.email}</p>)}
+                    <textarea
+                        name="message"
+                        id="message"
+                        placeholder="Message"
+                        rows="4"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    ></textarea>
+                    {hasSubmitted && (<p className="error">{errors.message}</p>)}
+                    <button
+                        disabled={hasSubmitted && Object.values(errors).length !== 0}
+                        className={buttonClass}
+                    >
+                        {buttonText}
+                    </button>
                 </form>
                 <div className="contact-icons">
                     <div>
