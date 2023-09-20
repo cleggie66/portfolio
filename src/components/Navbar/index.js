25683222b805f467d14ab97d5b8e7cfe4e2314c7
Caleb Cleghorn <caleb@cleghorn.org>
2023-09-20 10:16:47 -0500

    added letter generator

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useDispatch } from "react-redux"
 import { resetPage, setPage } from '../../store/viewing';
-import './Navbar.css';
 import { setProject } from '../../store/settings';
+import './Navbar.css';
 
 
 function Navbar() {
